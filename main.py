e560a5a9ae9ac575bcf8a597ef778232efbf035a
Peter Boucher <petebouch@gmail.com>
2021-02-01 08:17:27 +0100

    simplify lesson 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-fruit_and_veg = ["EGGS X24",
+shopping_list = ["EGGS X24",
                  "FRUIT",
                  "BANANAS",
                  "BERRIES",
@@ -17,8 +17,6 @@ fruit_and_veg = ["EGGS X24",
                  "SWEET POTS",
                  "FRESH PINEAPPLE",
                  "TOMS"]
-meat = ["MINI BURGERS", "CHICKEN BREAST"]
-shopping_list = [meat, fruit_and_veg]
 
 
 @app.route('/')
@@ -26,12 +24,6 @@ def index():
     return render_template('list.html', list=shopping_list)
 
 
-def print_shopping_list():
-    print(shopping_list)
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     pass
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
